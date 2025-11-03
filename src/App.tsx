c10a2b2f53111872098a5e8b0b905e8bbbc469d1
Trey Zima <trey.m.zima@gmail.com>
2025-11-02 18:20:21 -0700

    Testing URL

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
       <div className="content">
         <h2 className="script-font name-title">McKinsley's</h2>
         <h3 className="event-title">Winter One-derland</h3>
-        <p className="date">December 6th, 2027</p>
+        <p className="date">December 6th, 2025</p>
         <button className="rsvp-button" onClick={handleWillAttend}>
           Will attend
         </button>
